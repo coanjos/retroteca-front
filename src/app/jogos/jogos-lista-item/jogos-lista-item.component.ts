d11e6667e0ee319f8602f633f7dcc637c0cb30e7
Caio dos Anjos <coa.anjos@gmail.com>
2022-01-05 08:32:52 -0300

    Estiliza lista de jogos com componente filho; mexe no footer, mas não definitivamente

--- src/app/jogos/jogos-lista-item/jogos-lista-item.component.ts
+++ src/app/jogos/jogos-lista-item/jogos-lista-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { jogo } from '../interfaces/jogo';
 @Component({
   selector: 'app-jogos-lista-item',
   templateUrl: './jogos-lista-item.component.html',
   styleUrls: ['./jogos-lista-item.component.css']
 })
 export class JogosListaItemComponent implements OnInit {
+  @Input() jogo: jogo = { _id: '', titulo: '', descricao: '', ano: 0, capas: [], generos: [], autores: []};
+  @Output() apagarEvent = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  apagar(id: string) {
+    this.apagarEvent.emit(id);
   }
 
 }
